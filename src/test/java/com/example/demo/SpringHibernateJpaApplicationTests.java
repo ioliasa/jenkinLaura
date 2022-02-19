c52b99e18321802bcafb4a0366820e1591dc1832
Inma <ioliasa@gmail.com>
2022-02-19 13:29:23 +0100

    añadientdo test normal

--- src/test/java/com/example/demo/SpringHibernateJpaApplicationTests.java
+++ src/test/java/com/example/demo/SpringHibernateJpaApplicationTests.java
@@ -7,28 +7,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.model.Usuario;
 
+@SpringBootTest
+class JpaHibernateApplicationTests {
 
-@SpringBootTest(classes = SpringHibernateJpaApplicationTests.class)
-public class SpringHibernateJpaApplicationTests{
-	private Usuario u;
-	
-	
-	@Test
-	public void testGetContrasena() {
-		u = new Usuario("laura", "laura");
-		assertTrue("laura".equals(u.getContrasena()));
-	}
-	
-	@Test
-	public void testGetNombreUser() {
-		u = new Usuario("laura", "laura");
-		assertTrue("laura".equals(u.getNombreUser()));
-	}
-	
-	@Test
-	public void testGetDireccion() {
-		u = new Usuario("Laura", "laura", "calle Preciosa, 23, utrera, 41710");
-		assertTrue("calle Preciosa, 23, utrera, 41710".equals(u.getDireccion()));
-	}
 	
 }
+
+//@SpringBootTest(classes = SpringHibernateJpaApplicationTests.class)
+//public class SpringHibernateJpaApplicationTests{
+//	private Usuario u;
+//	
+//	
+//	@Test
+//	public void testGetContrasena() {
+//		u = new Usuario("laura", "laura");
+//		assertTrue("laura".equals(u.getContrasena()));
+//	}
+//	
+//	@Test
+//	public void testGetNombreUser() {
+//		u = new Usuario("laura", "laura");
+//		assertTrue("laura".equals(u.getNombreUser()));
+//	}
+//	
+//	@Test
+//	public void testGetDireccion() {
+//		u = new Usuario("Laura", "laura", "calle Preciosa, 23, utrera, 41710");
+//		assertTrue("calle Preciosa, 23, utrera, 41710".equals(u.getDireccion()));
+//	}
+//	
+//}
